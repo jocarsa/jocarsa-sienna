f2f3da61e9436e124410babaa1ee94dd157b6ff5
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-01-23 15:33:20 +0100

    colores en los elementos de repertorio

--- componentes/repositorio/repositorio.php
+++ componentes/repositorio/repositorio.php
@@ -1,11 +1,11 @@
 <div id="repositorio">
-    	<div class="elemento activo"></div>
-    	<div class="elemento"></div>
-    	<div class="elemento"></div>
-    	<div class="elemento"></div>
-    	<div class="elemento"></div>
-    	<div class="elemento"></div>
-    	<div class="elemento"></div>
+    	<div class="elemento activo" style="background:red;"></div>
+    	<div class="elemento" style="background:blue;"></div>
+    	<div class="elemento" style="background:green;"></div>
+    	<div class="elemento" style="background:cyan;"></div>
+    	<div class="elemento" style="background:magenta;"></div>
+    	<div class="elemento" style="background:yellow;"></div>
+    	<div class="elemento" style="background:orange;"></div>
     </div>
     
 <style>
